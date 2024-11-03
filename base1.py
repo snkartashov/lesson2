13c779ae7525c0b207239b775ed78127d976afba
snkartashov <fermyshka1701@gmail.com>
2024-11-03 20:24:40 +0300

    21.06

---
+++ base1.py
@@ -0,0 +1,21 @@
+print(5)
+print(type(5)) # int - integer
+print(5 + 5)
+print(5 - 5)
+print(5 * 5)
+print(5 / 5) # // - целочисленное деление, / - деление
+print(5 % 5) # % проверять четное ли число или нет например
+print(5 ** 6) # ** возведение в степень
+print(2.0)
+print(type(2.0))  #float дробные числа #int целые числа , числа не изменяемые типы
+print("hello world") # string - строка str
+print("hello, " + "world") #строки только сложения (concatenate)
+print("1" + "1") # ковычки=текст
+print("True False")
+print(type(True), type(False)) #bool тип данных верно/неверно boolean
+print(5 > 10)
+print(195 < 200, 1569<1655) # запятая - разделители
+print(5 >= 10, 5 <= 5, 5 == 5, 5 != 10) #and строгая "и"  or менее строгий оператор (хотя бы 1 выражение правдивое тогда тру)"или"
+print(5 != 10 or 5 > 10)
+print(14 != 17 and 13 < 17)
+print(type(int("17")))
