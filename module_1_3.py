4ab0584ffd40f44378785d8ee98883539cf54a8b
Сергей <fermyshka1701@gmail.com>
2024-11-09 19:32:41 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+
+name = "Sergey"
+print ("name:") , print( name )
+age = 29
+print ("age:") ,  print(age)
+new_age =( age + 1 )
+print ("new_age:") , print(new_age)
+is_student = True
+print ("is_student:") , print(is_student)
+
+
