8c7eda1cb020322798241d18a39d45b25174b270
Сергей <fermyshka1701@gmail.com>
2024-11-10 02:01:13 +0300

    Add files via upload
    Переменные

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+Number_of_homework_completed = 12
+time_spant =  1.5
+name_class = "Python"
+time_one_homework = (time_spant / Number_of_homework_completed)
+print("Курс:", name_class, ",всего задач:", Number_of_homework_completed, "затрачено часов:", time_spant, "среднее время выполнения", time_one_homework, "часа")
