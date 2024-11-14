3daafef199817799929e15b9e7da220a89a9b611
Сергей <fermyshka1701@gmail.com>
2024-11-15 00:06:15 +0300

    Add files via upload
    Практическое задание по теме: "Неизменяемые и изменяемые объекты. Кортежи и списки"

--- module_1_5.py
+++ module_1_5.py
@@ -1,6 +1,15 @@
-example = "Топинамбур"
-print(example[0])
-print(example[-1])
-print(example[5:])
-print(example[::-1])
-print(example[1:10:2])
+# example = "Топинамбур"
+# print(example[0])
+# print(example[-1])
+# print(example[5:])
+# print(example[::-1])
+# print(example[1:10:2])
+
+
+#Практическое задание по теме: "Неизменяемые и изменяемые объекты. Кортежи и списки"
+immutable_var = 1, 2, 3, 4, 5, 6, 'jww', 1.55 , 18, 'one'
+print(immutable_var)
+#immutable_var[0] = 7 #тип данных "tuple" нельзя изменять, поэтому закинем эту функцию тоже в комментарий
+mutable_list = [2002, "inst", 'а почему бы и нет?', "будет или", "не будет"]
+mutable_list[2] = "а потому что! :D "
+print(mutable_list)
