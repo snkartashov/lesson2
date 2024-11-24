1d4343104e65d668cfdf05cf54ef2ff1dba99cc6
Сергей <fermyshka1701@gmail.com>
2024-11-24 17:54:43 +0300

    Add files via upload
    Домашняя работа по уроку "Пространство имён"

---
+++ module_3_1.py
@@ -0,0 +1,26 @@
+# Домашняя работа по уроку "Пространство имён"
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    length = len(string)
+    upper = string.upper()
+    lower = string.lower()
+    return (length, upper, lower)
+
+def is_contains(string, list_to_search):
+    count_calls()
+    return string.lower() in (item.lower() for item in list_to_search)
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+
+
+print(calls)
